a042dfb31e79e391fa6d8affc863e1cc02299d5d
Taha <taha.aarian@gmail.com>
2018-07-11 00:29:11 +0430

    fixed bug

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -118,7 +118,7 @@ void MainWindow::connectToSignals() {
 }
 
 void MainWindow::initUi() {
-  setWindowTitle(QString(tr("FestivalCoin Wallet v1.0.0")).arg(Settings::instance().getVersion()));
+  setWindowTitle(QString(tr("FestivalCoin Wallet v1.0.1")).arg(Settings::instance().getVersion()));
 #ifdef Q_OS_WIN32
   createTrayIcon();
 #endif
